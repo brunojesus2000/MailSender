204dfe2ab5f41d4c105ac3580254d2e9c1bd1ddc
Bruno JP <brunojpsk8@gmail.com>
2016-03-06 17:55:31 -0300

    This is the initial commit with the itens contents

---
+++ MailSender_main.py
@@ -0,0 +1,7 @@
+# -*- coding: UTF-8 -*-
+#! /usr/bin/python
+
+#Este codigo vai varrer uma lista especifica e mandar um email a todos que estão nela
+
+import sys, os, smtplib
+
---
+++ README.md
@@ -0,0 +1,2 @@
+# MailSender
+This project send email to a specific list of emails, the objective is send comercial presentation in HTML
