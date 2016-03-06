cbb99585108c98f168016385524a5b4bb5efaad3
Bruno JP <brunojpsk8@gmail.com>
2016-03-06 18:43:10 -0300

    Add SMTP Connection, STMP Login and send 1st message

--- MailSender_main.py
+++ MailSender_main.py
@@ -5,3 +5,15 @@
 
 import sys, os, smtplib
 
+#Esta é a configuração de conexão com o servidor SMTP
+gmailSMTP = smtplib.SMTP('smtp.gmail.com', 587)
+gmailSMTP.ehlo()
+gmailSMTP.starttls()
+
+#Aqui você solicita os dados do usuário
+SenderEmail = raw_input('Digite seu email: ')
+SenderPwd = raw_input('Digite sua senha: ')
+gmailSMTP.login(SenderEmail,SenderPwd)
+
+gmailSMTP.sendmail(SenderEmail, 'brunojesus2000@yahoo.com.br', 'Subject: Teste PyMail \nEste é um teste para envio de email pelo Python ' )
+gmailSMTP.quit()
