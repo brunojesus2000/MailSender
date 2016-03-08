12abab31ede0c5450977927cbe4292ee020ce56f
Bruno JP <brunojpsk8@gmail.com>
2016-03-07 23:24:42 -0300

    Add feature with XLSX reader and FOR to send emails from the list

--- MailSender_main.py
+++ MailSender_main.py
@@ -3,9 +3,10 @@
 
 #Este codigo vai varrer uma lista especifica e mandar um email a todos que estão nela
 
-import sys, os, smtplib
+import sys, os, smtplib, openpyxl
 
-#Esta é a configuração de conexão com o servidor SMTP
+
+#Esta é a configuração de conexão com o servidor SMTP pelo modulo smtplib
 gmailSMTP = smtplib.SMTP('smtp.gmail.com', 587)
 gmailSMTP.ehlo()
 gmailSMTP.starttls()
@@ -15,5 +16,15 @@ SenderEmail = raw_input('Digite seu email: ')
 SenderPwd = raw_input('Digite sua senha: ')
 gmailSMTP.login(SenderEmail,SenderPwd)
 
-gmailSMTP.sendmail(SenderEmail, 'brunojesus2000@yahoo.com.br', 'Subject: Teste PyMail \nEste é um teste para envio de email pelo Python ' )
+#Aqui eu abro o documento xlsx pelo modulo openpyxl
+PlanContatos = openpyxl.load_workbook('/Users/bruno/Documents/plan_teste.xlsx')
+SheetContatos = PlanContatos.get_sheet_by_name('Plan1')
+#Laço que percorre as celulas da planilha pela coluna
+for cellObj in SheetContatos.columns[0]:
+    print "Enviando email para", cellObj.value
+    #Envio de modelo de email
+    gmailSMTP.sendmail(SenderEmail, cellObj.value, 'Subject: Teste PyMail \nEste é um teste para envio de email pelo Python , seu endereço foi cadastrado na minha planilha' )
+    print "Enviado com sucesso"
 gmailSMTP.quit()
+
+
